804b6585ce2f485c0e839c6acaa2514ed845f2ec
Gavin Song <86136270+gavin-St@users.noreply.github.com>
2023-09-16 00:37:30 -0400

    updated data models

--- tables/cameras.py
+++ tables/cameras.py
@@ -1,2 +1,10 @@
-class Camera():
-  1
+from extensions import db 
+
+class Camera(db.Model):
+  __bind_key__ = "cameras"
+  __tablename__='cameras'
+  id=db.Column(db.Integer, primary_key=True)
+  location=db.Column(db.String(40))
+
+  def __init__(self,location):
+    self.location = location
--- tables/users.py
+++ tables/users.py
@@ -1,5 +1,14 @@
 class recognizedFace():
-     1
+  __bind_key__ = "faces"
+  __tablename__='faces'
+  id=db.Column(db.Integer, primary_key=True)
+  haar_recognition=db.Array(db.Integer)
 
 class User():
-     2
+  __bind_key__ = "user"
+  __tablename__='user'
+  id=db.Column(db.Integer, primary_key=True)
+  face=db.Column(db.String(40))
+
+  def __init__(self,face):
+    self.face = recognizedFace(face)
